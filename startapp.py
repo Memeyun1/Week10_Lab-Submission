8d59d588ca4b96005f8b9a8a4de72cff233d94bb
Memeyun1 <memeyun5205@gmail.com>
2022-12-18 21:02:24 -0800

    add a page of starapp.py

---
+++ startapp.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/Week10_Lab-Submission")
+def hello_world():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000)
